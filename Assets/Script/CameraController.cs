8ea14f2011c1b664a6a8d24fd050e55109ba0128
박상빈 <qazw181900@naver.com>
2024-08-08 17:20:29 +0900

    chore: clipping
    robot & camera position clipping

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -14,6 +14,9 @@ public class CameraController : MonoBehaviour
     public float moveSpeed = 10f;
     public float lookSpeed = 2f;
     public float focusSpeed = 2f;
+    // 카메라의 위치 범위
+    public Vector2 xRange = new Vector2(7f, 42f);
+    public Vector2 zRange = new Vector2(4f, 40f);
 
     // 초기 X 사용자가 선택한 타겟
     public Transform target;
@@ -53,10 +56,16 @@ public class CameraController : MonoBehaviour
 
         // 카메라의 위치를 대상에서 오프셋만큼 떨어진 곳에 설정
         Vector3 desiredPosition = target.position - target.forward * Mathf.Abs(offset.z) + Vector3.up * offset.y;
+
+        // 카메라 위치를 범위 내로 제한
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, xRange.x, xRange.y);
+        desiredPosition.z = Mathf.Clamp(desiredPosition.z, zRange.x, zRange.y);
+
         // 카메라가 부드럽게 해당 좌표로 이동할 수 있도록 함.
         mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, desiredPosition, Time.deltaTime * focusSpeed);
 
         // 카메라가 항상 대상을 바라보도록 설정합니다.
         mainCamera.transform.LookAt(target.position);
     }
+   
 }
